7f226488467d0228d8cd7edd5828f7aeb12845ee
Md. Arshi Khan <arshikhan80708@gmail.com>
2025-12-25 00:53:52 +0530

    feat: add seller routes

--- server/Routes/sellerRoutes.js
+++ server/Routes/sellerRoutes.js
@@ -9,13 +9,11 @@ import {
 } from "../Controller/sellerController.js";
 
 const router = express.Router();
-
 //Crud
 router.get("/", getSellers);
 router.get("/:id", getSellerById);
 router.put("/:id", updateSeller);
 router.delete("/:id", deleteSeller);
-
 //Auth
 router.post("/register", registerSeller);
 router.post("/login", loginSeller);
