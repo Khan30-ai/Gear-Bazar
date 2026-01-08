e2553c7ba4a9ac2f7d1282a4b68ee23fae8a85f3
Md. Arshi Khan <arshikhan80708@gmail.com>
2026-01-08 23:01:42 +0530

    added new route of getProductById

--- server/Routes/productRoutes.js
+++ server/Routes/productRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { createdProduct,approveProduct,rejectProduct,getProducts } from "../Controller/productController.js";
+import { createdProduct,approveProduct,rejectProduct,getProducts, getProductById } from "../Controller/productController.js";
+import Seller from "../Models/Seller.js";
 
 const router = express.Router();
 
@@ -8,11 +9,14 @@ router.post("/",createdProduct);
 
 router.get("/", getProducts);
 
-
 //admin will approve product
 router.put("/:id/approve", approveProduct);
 
 //admin will reject product
 router.put("/:id/reject", rejectProduct);
+
+//getting product by id
+router.get("/:id", getProductById);
+
 
 export default router;
