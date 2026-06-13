3312cbcb17a3187b5b3b9ab91f0ad44f5da6b2bf
Md. Arshi Khan <arshikhan80708@gmail.com>
2026-06-13 23:26:49 +0530

    feat: add buyer seller and admin profile management

--- client/src/services/seller.service.js
+++ client/src/services/seller.service.js
@@ -4,3 +4,7 @@ export const getMySellerProfile = async () => {
     const response = await api.get("/sellers/profile");
     return response.data.seller;
 };
+export const updateMySellerProfile = async (data) => {
+    const response = await api.put("/sellers/profile", data)
+    return response.data.seller;
+}
